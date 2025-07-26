e8b69cd915bce9190d2192d3f5110842197df5df
Mangesh Thale <167566793+Mangeshthale@users.noreply.github.com>
2025-07-27 02:28:58 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -59,4 +59,5 @@ def serve_audio(filename):
 
 if __name__ == "__main__":
     print("🚀 Chatbot Server is Running...")
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host="0.0.0.0", port=port)
